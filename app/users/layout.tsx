f3244c2ed42b7a26471f08794a19ab312f34cec6
mhadziqrazin <mhadziq0403@gmail.com>
2024-08-22 23:11:34 +0700

    feat: user list

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,13 +1,18 @@
+import getUsers from "../actions/getUsers"
 import Sidebar from "../components/sidebar/Sidebar"
+import UserList from "./components/UserList"
 
-export default function UsersLayout({
+export default async function UsersLayout({
   children
 }: {
   children: React.ReactNode
 }) {
+  const users = await getUsers()
+
   return (
     <Sidebar>
       <div className="h-full">
+        <UserList users={users} />
         {children}
       </div>
     </Sidebar>
