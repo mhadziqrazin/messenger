23f2391bcad2efba858cf2fbc95ae316d0c4404b
mhadziqrazin <mhadziq0403@gmail.com>
2024-08-29 15:35:08 +0700

    feat: loading state

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Avatar from "@/app/components/Avatar";
+import LoadingModal from "@/app/components/LoadingModal";
 import { User } from "@prisma/client";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -27,21 +28,26 @@ const UserBox: React.FC<UserBoxProps> = (props) => {
     .finally(() => setIsLoading(false))
   }, [props.data, router])
   return (
-    <button
-      onClick={handleClick}
-      className="w-full relative flex items-center space-x-3 text-left bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
-    >
-      <Avatar user={props.data} />
-      <div className="min-w-0 flex-1">
-        <div className="focus:outline-none">
-          <div className="flex justify-between items-center mb-1">
-            <p className="text-sm font-medium text-gray-900">
-              {props.data.name}
-            </p>
+    <>
+      {isLoading && (
+        <LoadingModal />
+      )}
+      <button
+        onClick={handleClick}
+        className="w-full relative flex items-center space-x-3 text-left bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
+      >
+        <Avatar user={props.data} />
+        <div className="min-w-0 flex-1">
+          <div className="focus:outline-none">
+            <div className="flex justify-between items-center mb-1">
+              <p className="text-sm font-medium text-gray-900">
+                {props.data.name}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </button>
+      </button>
+    </>
   )
 }
 
